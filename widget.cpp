3d22a44c05a52a988339678193d9442af0f0ad49
OlgaBolkova <olchick.borovikova@yandex.ru>
2018-03-29 03:55:13 +0300

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -7,6 +7,8 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
+         PlayList.setFileName("PlayList.txt");
+ PlayList.open(QIODevice::ReadWrite|QIODevice::Text);
     ui->setupUi(this);
     // Настройка таблицы плейлиста
     m_playListModel = new QStandardItemModel(this);
@@ -16,11 +18,27 @@ Widget::Widget(QWidget *parent) :
                                                             << tr("File Path"));
 
     ui->playlistView->setEditTriggers(QAbstractItemView::NoEditTriggers);   // Отключаем редактирование
+        ui->playlistView->hideColumn(1);    // Скрываем колонку, в которой хранится путь к файлу
+     ui->playlistView->verticalHeader()->setVisible(false);                  // Скрываем нумерацию строк
+     ui->playlistView->setSelectionBehavior(QAbstractItemView::SelectRows);  // Включаем выделение строк
     // Включаем подгонку размера последней видимой колонки к ширине TableView
     ui->playlistView->horizontalHeader()->setStretchLastSection(true);
 
     m_player = new QMediaPlayer(this);          // Инициализируем плеер
     m_playlist = new QMediaPlaylist(m_player);  // Инициализируем плейлист
+         m_player->setPlaylist(m_playlist);          // Устанавливаем плейлист в плеер
+ m_player->setVolume(70);                    // Устанавливаем громкость воспроизведения треков
+ m_playlist->setPlaybackMode(QMediaPlaylist::Loop);  // Устанавливаем циклический режим проигрывания плейлиста
+ Player();
+   // подключаем кнопки управления к слотам управления
+   // Здесь отметим, что навигация по плейлисту осуществляется именно через плейлист
+
+   connect(ui->btn_play, &QToolButton::clicked, m_player, &QMediaPlayer::play);
+    connect(ui->btn_stop, &QToolButton::clicked, m_player, &QMediaPlayer::stop);
+    // При даблклике по треку в таблице устанавливаем трек в плейлисте
+        connect(ui->playlistView, &QTableView::doubleClicked, [this](const QModelIndex &index){
+            m_playlist->setCurrentIndex(index.row());
+        });
  }
 
 Widget::~Widget()
@@ -39,6 +57,7 @@ void Widget::on_btn_add_clicked()
                                                       tr("Open files"),
                                                       QString(),
                                                       tr("Audio Files (*.mp3)"));
+     QTextStream stream(&PlayList);
 
     // Далее устанавливаем данные по именам и пути к файлам
     // в плейлист и таблицу отображающую плейлист
@@ -47,6 +66,27 @@ void Widget::on_btn_add_clicked()
         items.append(new QStandardItem(QDir(filePath).dirName()));
         items.append(new QStandardItem(filePath));
         m_playListModel->appendRow(items);
+         stream << filePath << "\r\n";
         m_playlist->addMedia(QUrl(filePath));
     }
+}
+void Widget:: Player(){
+    if (PlayList.isOpen()){
+        QString line;
+        QTextStream stream(&PlayList);//
+        while (!stream.atEnd()){
+                  line+=stream.readLine()+"\n";
+               }
+        QStringList lines = line.split( "\n", QString::SkipEmptyParts );
+
+
+     foreach (QString filePath, lines) {
+
+        QList<QStandardItem *> items;
+        items.append(new QStandardItem(QDir(filePath).dirName()));
+        items.append(new QStandardItem(filePath));
+        m_playListModel->appendRow(items);
+        m_playlist->addMedia(QUrl(filePath));
+    }
+   }
 }
